1a71d60fc211f7f2be96f026281facb234426eff
Eric Lima <eric.george.lima@gmail.com>
2025-05-10 16:08:14 +0100

    agenda

---
+++ Agenda/src/main/java/pt/ipcb/mei/aid/agenda/AgendaApplication.java
@@ -0,0 +1,13 @@
+package pt.ipcb.mei.aid.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaApplication.class, args);
+    }
+
+}
