f4275f99be69c497dcc8cdebabe17f04a8c33cf8
Eric Lima <eric.george.lima@gmail.com>
2025-05-11 02:33:11 +0100

    desabilitei o https

---
+++ README.md
@@ -0,0 +1,12 @@
+adicionar no ficheiro host (para debug)
+
+
+127.0.0.1 postgres
+127.0.0.1 agenda
+127.0.0.1 user
+127.0.0.1 receitas
+127.0.0.1 estoque
+127.0.0.1 medicamentos
+
+127.0.0.1 name-server
+127.0.0.1 cloud-gateway
--- User/src/main/java/pt/ipcb/mei/aid/user/UserApplication.java
+++ User/src/main/java/pt/ipcb/mei/aid/user/UserApplication.java
@@ -3,6 +3,7 @@ package pt.ipcb.mei.aid.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UserApplication {
 
