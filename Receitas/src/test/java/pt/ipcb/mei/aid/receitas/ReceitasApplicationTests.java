29f74d848fb8f640f3c68253f831635a77671c8c
Eric Lima <eric.george.lima@gmail.com>
2025-05-11 04:32:45 +0100

    receitas

---
+++ Receitas/src/test/java/pt/ipcb/mei/aid/receitas/ReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ipcb.mei.aid.receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
