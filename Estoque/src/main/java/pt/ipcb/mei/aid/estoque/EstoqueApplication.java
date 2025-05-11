7c11e270b6c90df7c3ae5a1d83134f1471812595
Eric Lima <eric.george.lima@gmail.com>
2025-05-11 04:49:22 +0100

    estoque

---
+++ Estoque/src/main/java/pt/ipcb/mei/aid/estoque/EstoqueApplication.java
@@ -0,0 +1,13 @@
+package pt.ipcb.mei.aid.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoqueApplication.class, args);
+    }
+
+}
