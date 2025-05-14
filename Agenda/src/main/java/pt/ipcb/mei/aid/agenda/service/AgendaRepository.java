aa75141a8cf4fd5fd4d9b533f4c113028bac3526
Eric Lima <eric.george.lima@gmail.com>
2025-05-14 09:49:40 +0100

    adding agenda

--- Agenda/src/main/java/pt/ipcb/mei/aid/agenda/service/AgendaRepository.java
+++ Agenda/src/main/java/pt/ipcb/mei/aid/agenda/service/AgendaRepository.java
@@ -1,4 +1,13 @@
 package pt.ipcb.mei.aid.agenda.service;
 
-public interface AgendaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pt.ipcb.mei.aid.agenda.model.Agenda;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+
+    List<Agenda> findByUserIdAndInicioBetween(Long userId, LocalDateTime inicio, LocalDateTime fin);
+
 }
--- Agenda/src/main/java/pt/ipcb/mei/aid/agenda/service/AgendaService.java
+++ Agenda/src/main/java/pt/ipcb/mei/aid/agenda/service/AgendaService.java
@@ -1,4 +1,42 @@
 package pt.ipcb.mei.aid.agenda.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.ipcb.mei.aid.agenda.model.Agenda;
+
+import java.time.*;
+import java.util.List;
+
+@Service
 public class AgendaService {
+
+    private AgendaRepository agendaRepository;
+
+    public AgendaService(AgendaRepository agendaRepository) {
+        this.agendaRepository = agendaRepository;
+    }
+
+    public List<Agenda> buscarEventosDoDia(Long userId, LocalDate data) {
+        LocalDateTime inicioDoDia = data.atStartOfDay();
+        LocalDateTime fimDoDia = data.atTime(LocalTime.MAX);
+        return agendaRepository.findByUserIdAndInicioBetween(userId, inicioDoDia, fimDoDia);
+    }
+
+    public List<Agenda> buscarEventosDaSemana(Long userId, LocalDate data) {
+        LocalDate primeiroDia = data.with(DayOfWeek.MONDAY);
+        LocalDate ultimoDia = data.with(DayOfWeek.SUNDAY);
+        LocalDateTime inicioSemana = primeiroDia.atStartOfDay();
+        LocalDateTime fimSemana = ultimoDia.atTime(LocalTime.MAX);
+        return agendaRepository.findByUserIdAndInicioBetween(userId, inicioSemana, fimSemana);
+    }
+
+    public List<Agenda> buscarEventosDoMes(Long userId, YearMonth mes) {
+        LocalDate primeiroDia = mes.atDay(1);
+        LocalDate ultimoDia = mes.atEndOfMonth();
+        LocalDateTime inicioMes = primeiroDia.atStartOfDay();
+        LocalDateTime fimMes = ultimoDia.atTime(LocalTime.MAX);
+        return agendaRepository.findByUserIdAndInicioBetween(userId, inicioMes, fimMes);
+    }
+
+
 }
