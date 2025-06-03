0e26c7971830ab90097475b3593a6c04f8dc8b07
Eric Lima <eric.george.lima@gmail.com>
2025-06-03 07:10:41 +0100

    final

--- Agenda/src/main/java/pt/ipcb/mei/aid/agenda/repository/AgendaRepository.java
+++ Agenda/src/main/java/pt/ipcb/mei/aid/agenda/repository/AgendaRepository.java
@@ -1,4 +1,4 @@
-package pt.ipcb.mei.aid.agenda.service;
+package pt.ipcb.mei.aid.agenda.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pt.ipcb.mei.aid.agenda.model.Agenda;
